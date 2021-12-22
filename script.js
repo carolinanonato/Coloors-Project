525824caa2e31a8c0c632486699fe412d7c3c518
Carolina Nonato <csnonato@gmail.com>
2021-12-22 17:46:08 -0500

    Fixing Broken Slides

--- script.js
+++ script.js
@@ -45,9 +45,16 @@ function generateHex() {
 
 
 function randomColors() {
+    
+    initialColors = [];
+
     colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
         const randomColor = generateHex();
+
+        //add it to the array
+
+        initialColors.push(chroma(randomColor).hex());
 
         //add the color to the bg
         div.style.backgroundColor = randomColor;
@@ -104,7 +111,7 @@ function hslControls(e){
    const brightness = sliders[1];
    const saturation = sliders[2];
 
-    const bgColor = colorDivs[index].querySelector('h2').innerText;
+    const bgColor = initialColors[index];
    
     let color = chroma(bgColor)
         .set('hsl.s', saturation.value)
