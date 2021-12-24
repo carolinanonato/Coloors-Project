a602205b7f3440d5b764223f52513199a0a6e608
Carolina Nonato <csnonato@gmail.com>
2021-12-23 20:31:15 -0500

    Copy To Clipboard

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2")
-
+const popup = document.querySelector('.copy-container')
 let initialColors;
 
 
@@ -20,7 +20,17 @@ colorDivs.forEach((div, index) => {
 })
 
 
+currentHexes.forEach(hex => {
+    hex.addEventListener('click', () => {
+        copyToClickboard(hex);
+    })
+})
 
+popup.addEventListener('transitionend', () => {
+    const popupBox = popup.children[0];
+    popup.classList.remove('active');
+    popupBox.classList.remove('active');
+})
 
 
 //Functions
@@ -170,5 +180,20 @@ function resetInputs () {
         });
 
     }
+
+function copyToClickboard (hex) {
+    const el = document.createElement('textarea');
+    el.value = hex.innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el)
+
+    //Pop up animation
+    const popupBox = popup.children[0];
+    popup.classList.add("active")
+    popupBox.classList.add("active");
+}
+
 
 randomColors();
